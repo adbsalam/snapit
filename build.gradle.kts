15a5523840a98f98dfdc13d170b04d9e9baa6c04
adb_salam <39949965+MuhammadAbdulSalam@users.noreply.github.com>
2023-07-19 15:43:02 +0100

    updates sample for snapit-plugin-1.0.5 (#14)
    Co-authored-by: Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.kotlin) apply false
+    alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.paparazzi) apply false
-    alias(libs.plugins.maven.publish) apply false
+    alias(libs.plugins.snapit) apply false
     alias(libs.plugins.kover) apply false
 }
 
